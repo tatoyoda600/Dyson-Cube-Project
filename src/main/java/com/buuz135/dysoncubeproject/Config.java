f6394a35c6aa895216c8ab01a08b5dccd0fd420b
tatoyoda600 <26845479+tatoyoda600@users.noreply.github.com>
2026-05-10 02:11:56 -0300

    Fix wrong bool type
    bool -> boolean

--- src/main/java/com/buuz135/dysoncubeproject/Config.java
+++ src/main/java/com/buuz135/dysoncubeproject/Config.java
@@ -37,6 +37,6 @@ public class Config {
     @ConfigVal.InRangeInt(min = 1)
     public static int RAIL_EJECTOR_CONSUME = 40;
 
-    @ConfigVal(comment = "Set to 'true' to require power for the em railejector to semd items")
-    public static bool RAIL_EJECTOR_REQUIRES_POWER = false;
+    @ConfigVal(comment = "Set to 'true' to require power for the em railejector to send items")
+    public static boolean RAIL_EJECTOR_REQUIRES_POWER = false;
 }
